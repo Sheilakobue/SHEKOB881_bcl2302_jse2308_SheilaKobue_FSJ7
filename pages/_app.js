9910d04c8dbcda6c93daed8dd145b097f82c048c
Sheila Kobue <maserole.mojela@gmail.com>
2023-09-20 11:51:43 +0200

    created files and deleted a file

--- pages/_app.js
+++ pages/_app.js
@@ -1,26 +1,25 @@
 import Head from 'next/head';
-import Layout from '../components/layout/layout';
-import Notification from '../components/ui/notification';
 
+import Layout from '../components/layout/layout';
+import { NotificationContextProvider } from '../store/notification-context';
 import '../styles/globals.css';
 
-export default function MyApp({ Component, pageProps }) {
+function MyApp({ Component, pageProps }) {
   return (
-   
+    <NotificationContextProvider>
       <Layout>
         <Head>
           <title>Next Events</title>
-          <meta name="description" content="NextJS Events" />
+          <meta name='description' content='NextJS Events' />
           <meta
-            name="viewport"
-            content="initial-scale=1.0, width=device-width"
+            name='viewport'
+            content='initial-scale=1.0, width=device-width'
           />
         </Head>
         <Component {...pageProps} />
-        <Notification />
       </Layout>
-    
+    </NotificationContextProvider>
   );
 }
 
-
+export default MyApp;
